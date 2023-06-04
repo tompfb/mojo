7e8bc0b094073ed5c7a5f8804955444e47cf9111
sattaponz <sattakit.kacs60@ubru.ac.th>
2023-06-05 02:41:58 +0700

    add about -readmore map

--- about.php
+++ about.php
@@ -15,11 +15,91 @@
     <link rel="icon" href="../favicon.webp" type="image/x-icon" />
     <link rel="apple-touch-icon" href="../favicon.webp" />
     <?php include('./import-css.php'); ?>
+
+    <!-- Include jQuery -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+
+    <script src="../js/readmore.js"></script>
+    <link rel="stylesheet" href="../js/readmore.css">
+
+    <style>
+        @media screen and (max-width:992px) {
+            .iframe_loca {
+                width: 400px !important;
+            }
+        }
+
+        a img.img-fluid {
+            aspect-ratio: 3/2;
+            object-fit: contain;
+        }
+    </style>
+
 </head>
 
 <body>
     <?php include('./component/header.php'); ?>
 
+    <article>
+        <section class="site-main-content">
+            <div class="container text-center">
+
+                <div class="read-more">
+                    <h1 class="txt-heading text-start">mojo esan</h1>
+                    <p class="text-start">
+                        ศูนย์สื่อสุขภาวะเพื่อการสื่อสารภาคอีสาน (MOJO อีสาน) หรือ Esan Mobile Journalist เป็นโครงการที่ดำเนินงานโดยมูลนิธิสื่อสร้างสุข ได้รับการสนับสนุนงบประมาณจากสำนักงานกองทุนสนับสนุนสุขภาพ (สสส.) ภายใต้แผนงานสำนักสร้างเสริมระบบสื่อและสุขภาวะทางปัญญา (สำนัก 11) มีเป้าหมายดำเนินงานเพื่อให้เกิดการใช้สื่ออย่างรู้เท่าทันและปลอดภัย  เพราะในยุคที่ทุกคนเป็นได้ทั้งผู้ผลิตและผู้รับสาร (media user and creators) สื่อจึงเป็นเสมือนดาบสองคมที่จะสามารถทำให้เกิดคุณและโทษ  ดังนั้นการสร้างความตระหนักต่อความเข้าใจในบริบทสื่อใหม่  จึงเป็นการทำให้ทุกคนทั้งผู้ที่ส่งสารและรับสารสามารถเข้าถึง เข้าใจ ตีความ ประเมิน และสร้างข้อมูล และสื่อในรูปแบบที่หลากหลายด้วยความตระหนักถึงผลกระทบของข้อมูลและสื่อต่าง ๆโดยไม่ถูกครอบงำที่จะสามารถใช้สื่อให้เป็นประโยชน์ต่อการเรียนรู้และการดำรงชีวิตทั้งของตนเอง ครอบครัว ชุมชน  และสังคม 
+                    </p>
+
+                    <p class="text-start">
+                        ทั้งนี้ มูลนิธิสื่อสร้างสุขอุบลราชธานี เป็นองค์กรสาธารณะประโยชน์ที่ทำงานด้านสื่อ ซึ่งได้จดทะเบียนมูลนิธิและเริ่มทำงานร่วมกับชุมชนในพื้นที่ภาคอีสานมาตั้งแต่ปี 2556 ที่ผ่านมาได้ทำงานในการสนับสนุนชุมชนและกลุ่มองค์กรภาคประชาสังคม รวมถึงหน่วยงานราชการต่างๆโดยใช้สื่อมัลติมีเดียเป็นเครื่องมือเพื่อสร้างกระบวนการมีส่วนร่วมของสังคมในการสะท้อนสังคมในทุกมิติ  ทั้งกระบวนการแก้ปัญหาในระดับชุมชน  การสร้างแนวทางการมีส่วนร่วมในสังคมระดับเมืองและรวมถึงการนำเสนอเรื่องราวดีๆที่เกิดขึ้นในท้องถิ่น  จนนำไปสู่การสร้างบรรทัดฐานที่ดีให้สังคมในหลายๆประเด็น  อาทิ โครงการสื่อสารสาธารณะเพื่อเสริมพลังชุมชน โครงการการใช้สื่อในกลุ่มผู้สูงวัยเพื่อการเปลี่ยนแปลงสังคม  โครงการสะพานเสริมสร้างประชาธิปไตย  โครงการนักจัดรายการวิทยุปอดสะอาด โครงการใช้สื่อเพื่อรณรงค์การไม่ดื่มแอลกอฮอล์  โครงการทีวีดิจิตอลชุมชน  โครงการกินสบายใจเพื่อส่งเสริมสุขภาวะและความมั่นคงทางอาหาร  โครงการอีสานโคแฟค : หยุดข่าวลวงทวงความจริง  และยังมีโครงการอื่นๆอีกหลายโครงการ  นอกจากนี้ยังเป็นทีมผลิตสื่อที่ร่วมทำงานกับสำนักเครือข่ายสื่อสาธารณะ ไทยพีบีเอส  ซึ่งได้ทำงานเพื่อการสื่อสารสาธารณะโดยใช้สื่อเป็นเครื่องมือในการสร้างความตระหนักทางสังคมเพื่อสร้างกลไกเฝ้าระวังภาคประชาชน ภาคประชาสังคม  ร่วมกับภาครัฐและภาควิชาการให้เกิดการรู้เท่าทันสื่อให้กลายเป็นพลเมืองดิจิทัล (digital citizenship) และในช่วงปี 2564 ที่ผ่านมายังได้ทำงานโครงการนำร่องภายใต้เพจอุบลคอนเน็ก  ร่วมกับคุณสุทธิชัย  หยุ่น  ผ่านหลักสูตร “Suthichai Live Mojo Lab”  ทั้งนี้เพื่อให้เกิดการสร้างนักสื่อสารที่มีจรรยาบรรณและการเป็นผู้ผลิตสื่อชุมชนที่สามารถออกแบบการเนื้อหาและการเล่าเรื่องที่สร้างสรรค์  ที่ถือเป็นการสร้างระบบนิเวศสื่อสุขภาวะ
+                    </p>
+                    <p class="text-start">
+                        ดังนั้น จากข้อมูลสถานการณ์ปัญหาภายใต้บริบทสื่อใหม่และแนวทางการขับเคลื่อนงานของสำนักสร้างเสริมระบบสื่อและสุขภาวะทางปัญญา และผนวกกับประสบการณ์การทำงานของมูลนิธิสื่อสร้างสุข จึงเป็นที่มาของการริเริ่มโครงการศูนย์สื่อสุขภาวะเพื่อการสื่อสารภาคอีสาน โดยมีเป้าหมายการทำงานคือการสร้างคนและชุมชนรู้เท่าทันสื่อ ให้เกิดจริยธรรมการสื่อสารและสามารถสร้างสื่อในเชิงลึก  โดยการทำงานร่วมกับกลุ่มเป้าหมายที่ครอบคลุมทั้งเด็ก  เยาวชน  ผู้ใหญ่ และผู้สูงอายุที่เคยทำงานร่วมกับมูลนิธิฯ ไม่ว่าจะเป็นเครือข่ายนักสื่อสารทีวีดิจิตอลชุมชน  เครือข่ายอีสานโคแฟค และเครือข่ายอื่นๆ  โดยใช้ทั้งพื้นที่เดิมที่เคยทำงานและการเปิดโอกาสให้พื้นที่ใหม่ที่เป็นภาคีเข้ามาร่วมใช้พื้นที่ในการสร้างสรรค์ร่วมกัน ซึ่งมี 6 จังหวัด  ได้แก่  อุบลราชธานี  อำนาจเจริญ  ศรีสะเกษ  สุรินทร์  มหาสารคาม  และขอนแก่น 
+                    </p>
+                    <p class="text-start">
+                        ติดต่อเรา <br>
+                        โครงการศูนย์สื่อสุขภาวะเพื่อการสื่อสารภาคอีสาน <br>
+                        สำนักงานมูลนิธิสื่อสร้างสุข <br>
+                        330/5 หมู่ 17 ตำบลไร่น้อย อำเภอเมือง จังหวัดอุบลราชธานี <br>
+                        โทร 087- 8766413 / 085 - 4157901 / 099 - 8980280 <br>
+                    </p>
+                    <p class="text-start">
+                        ภาคี <br>
+
+                        <a href="https://www.thaihealth.or.th" target="_blank" rel="ugc">https://www.thaihealth.or.th </a><br>
+                        <a href="https://thecitizen.plus" target="_blank" rel="ugc">https://thecitizen.plus </a><br>
+                        <a href="https://www.facebook.com/juiorn" target="_blank" rel="ugc">https://www.facebook.com/juiorn </a><br>
+                        <a href="https://www.facebook.com/Ginsabaijai" target="_blank" rel="ugc">https://www.facebook.com/Ginsabaijai </a><br>
+                        <a href="https://www.facebook.com/UbonConnect" target="_blank" rel="ugc">https://www.facebook.com/UbonConnect </a><br>
+                        <a href="https://www.facebook.com/search/top/?q=สื่อสร้างสุข" target="_blank" rel="ugc">https://www.facebook.com/search/top/?q=สื่อสร้างสุข</a> <br>
+
+                    </p>
+                    <p class="prompt">
+                        <a class="button btn btn-primary" href="#">ดูเพิ่มเติม</a>
+                    </p>
+                </div>
+            </div>
+            <div class="container bg-white">
+                <div class="d-block my-3 text-center">
+                    <a href="https://www.thaihealth.or.th" class="text-decoration-none d-inline-block my-2 mx-3" target="_blank" title="สสส."><img data-src="../img/Logo-sss.png" class="img-fluid lazy zoom-hover" width="180" height="165" alt=""></a>
+                    <a href="https://thecitizen.plus" class="text-decoration-none d-inline-block my-2 mx-3" target="_blank" title="thecitizen"><img data-src="../img/Logo-thecitizen.png" class="img-fluid lazy zoom-hover" width="380" height="165" alt=""></a>
+                    <a href="https://www.facebook.com/UbonConnect" class="text-decoration-none d-inline-block my-2 mx-3" target="_blank" title="ubon connect"><img data-src="../img/Logo-Ubon-Connect.png" class="img-fluid lazy zoom-hover" width="110" height="200" alt=""></a>
+                    <a href="https://www.facebook.com/UbonConnect" class="text-decoration-none d-inline-block my-2 mx-3" target="_blank" title="สื่อสร้างสุขอุบลราชธานี"><img data-src="../img/Logo-medie.png" class="img-fluid lazy zoom-hover" width="110" height="200" alt=""></a>
+                    <a href="https://www.facebook.com/juiorn" class="text-decoration-none d-inline-block my-2 mx-3" target="_blank" title="ห้องสมุดแมวหางกิ้นส์"><img data-src="../img/Logo-eat.png" class="img-fluid lazy zoom-hover" width="110" height="200" alt=""></a>
+                    <a href="https://www.facebook.com/Ginsabaijai" class="text-decoration-none d-inline-block my-2 mx-3" target="_blank" title="กินสบายใจ"><img data-src="../img/Logo-cat.png" class="img-fluid lazy zoom-hover" width="110" height="200" alt=""></a>
+                </div>
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d3787.650207196164!2d99.40087488255618!3d18.317597916183807!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sth!2sth!4v1685907379962!5m2!1sth!2sth" width="1290" height="550" class="iframe_loca" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+            </div>
+        </section>
+    </article>
+
+    <script>
+        $(".read-more").readMore({
+            previewHeight: 300
+        });
+    </script>
 
     <?php include('./component/footer.php'); ?>
     <?php include('./import-js.php'); ?>
