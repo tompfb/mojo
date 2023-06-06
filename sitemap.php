8e6ee9c94e9475e03a3a7f043e42ad3030f8ed3e
tompfb <kritsana6130@gmail.com>
2023-06-06 22:32:48 +0700

    sitemap

--- sitemap.php
+++ sitemap.php
@@ -3,5 +3,34 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     <!--  created with Free Online Sitemap Generator www.xml-sitemaps.com  -->
- 
+    <url>
+        <loc>https://www.mojoesan.com/</loc>
+        <lastmod>2023-06-06T15:30:47+00:00</lastmod>
+        <priority>1.00</priority>
+    </url>
+    <url>
+        <loc>https://www.mojoesan.com/interview/</loc>
+        <lastmod>2023-06-06T15:30:47+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://www.mojoesan.com/research/</loc>
+        <lastmod>2023-06-06T15:30:47+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://www.mojoesan.com/podcast/</loc>
+        <lastmod>2023-06-06T15:30:47+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://www.mojoesan.com/esan-clip/</loc>
+        <lastmod>2023-06-06T15:30:47+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://www.mojoesan.com/about/</loc>
+        <lastmod>2023-06-06T15:30:47+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
 </urlset>
